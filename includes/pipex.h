daee59c02e9b898c0526a3839dd637091831a279
Oussama DX <98095867+OussamaDX@users.noreply.github.com>
2022-12-19 22:56:50 +0100

    Update pipex.h

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: ooussaad <ooussaad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 13:44:05 by ooussaad          #+#    #+#             */
-/*   Updated: 2022/12/18 21:25:56 by ooussaad         ###   ########.fr       */
+/*   Updated: 2022/12/19 18:15:43 by ooussaad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@
 void	error();
 void	excute_cmd1(char **argv,char **envp);
 char	**find_path(char **envp);
-
+void	excute_cmd2(char **argv,char **envp);
 
 #endif
