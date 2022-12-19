e7baaf51369cb857ecd0f4ce54f1f174a4ae582c
Oussama DX <98095867+OussamaDX@users.noreply.github.com>
2022-12-19 22:55:49 +0100

    Update pipex.c

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: ooussaad <ooussaad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 16:26:58 by ooussaad          #+#    #+#             */
-/*   Updated: 2022/12/18 21:28:44 by ooussaad         ###   ########.fr       */
+/*   Updated: 2022/12/19 22:42:30 by ooussaad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,30 @@
 void	child_process(char **argv, char **envp, int *fd)
 {
 	int		fout;
-
 	fout = open(argv[1], O_RDONLY, 0777);
 	if (fout == -1)
 		error();
- 	dup2(fd[1], 0);
- 	dup2(fout, 1);
+ 	dup2(fd[1], 1);
+ 	dup2(fout, 0);
  	close(fd[0]);
  	excute_cmd1((char **)argv[2], envp);
 }
 
-
-
+void parent_process(char **argv, char **envp,int *fd)
+{
+	int fout2;
+	fout2 = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fout2 == -1)
+        error();
+	dup2(fd[0], 0);
+	dup2(fout2, 1);
+	close(fd[1]);
+	excute_cmd2((char **)argv[3], envp);
+}
 int	main(int argc, char **argv, char **envp)
 {
 	int		fd[2];
-	pid_t	pid1;
+	int	pid1;
 
 	if (argc == 5)
 	{
@@ -41,5 +49,9 @@ int	main(int argc, char **argv, char **envp)
 			error();
 		if (pid1 == 0)
 			child_process(argv, envp, fd);
+		waitpid(pid1, NULL, 0);
+		parent_process (argv,envp,fd);
 	}
+	else
+		error (127);
 }
