9020e87b4c242a288abdb211e861abe93f46930c
Oussama DX <98095867+OussamaDX@users.noreply.github.com>
2022-12-21 22:42:52 +0100

    Update pipex.c

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: ooussaad <ooussaad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/15 16:26:58 by ooussaad          #+#    #+#             */
-/*   Updated: 2022/12/20 15:55:03 by ooussaad         ###   ########.fr       */
+/*   Updated: 2022/12/21 22:36:56 by ooussaad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 void	child_process(char **argv, char **envp, int *fd)
 {
 	int		fin;
-	if (access(argv[1], F_OK) == - 1)
+	if (access(argv[1], F_OK) == - 1) //if cmd 1 not found
 	{
 		write(2, "no such file or directory", 20);
 		exit(1);
 	}
-	fin = open(argv[1], O_RDONLY, 0777);
- 	dup2(fd[1], 1);
- 	dup2(fin, 0);
- 	close(fd[0]);
- 	excute_cmd(argv[2], envp);
+	fin = open(argv[1], O_RDONLY, 0777); // we should open a file and read it
+	dup2(fd[1], 1); // ||1 = output ||
+	dup2(fin, 0); // ||0 = input  ||
+	close(fd[0]);
+	excute_cmd(argv[2], envp);
 }
 
 void parent_process(char **argv, char **envp,int *fd)
@@ -45,13 +45,14 @@ int	main(int argc, char **argv, char **envp)
 {
 	int		fd[2];
 	int	pid1;
+	int pid2;
 
 	if (argc == 5)
 	{
 		if (pipe(fd) == -1)
 		{
 			write (1, "no piping", 9);
-			return (1);
+			return (0);
 		}
 		pid1 = fork();
 		if (pid1 == -1)
@@ -60,10 +61,20 @@ int	main(int argc, char **argv, char **envp)
 		}
 		if (pid1 == 0)
 			child_process(argv, envp, fd);
-		waitpid(pid1, NULL, 0);
-		parent_process (argv,envp,fd);
+		else
+		{
+			pid2 = fork();
+			if (pid2 == -1)
+            {
+				write(2, "no child", 8);
+			}
+			if (pid2 == 0)
+                parent_process(argv, envp, fd);
+			else
+				waitpid(pid1, NULL, 0);
+		}
+
 	}
 	else
 		ft_putstr ("please entre 5 arguments \n");
 }
-
